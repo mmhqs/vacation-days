f3ee81a78e0e688d5c1028638f3a5fee79dde7f9
mmhqs <112990361+mmhqs@users.noreply.github.com>
2025-11-26 19:36:07 -0300

    feat: add first components

---
+++ code/vacation-days-react-project/src/components/form.tsx
@@ -0,0 +1,7 @@
+export const Form = () => { 
+    return (
+        <div>
+            <p>Qual seu estado?</p>
+        </div>
+    )
+}
---
+++ code/vacation-days-react-project/src/components/main-title.tsx
@@ -0,0 +1,8 @@
+export const MainTitle = () => {
+  return (
+    <>
+      <h1>Escolha o melhor período para suas férias!</h1>
+      <p>Descanse, trabalhador cansado, descanse.</p>
+    </>
+  );
+};
