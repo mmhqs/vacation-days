7aaa9ddeb9f98c382ca078cc06c2d7e7b2dd3c9b
mmhqs <112990361+mmhqs@users.noreply.github.com>
2025-11-26 21:28:15 -0300

    config: tailwind with vite

--- code/vacation-days-react-project/src/components/main-title.tsx
+++ code/vacation-days-react-project/src/components/main-title.tsx
@@ -2,9 +2,9 @@
 export const MainTitle = () => {
   return (
     <>
-      <div className="h-screen flex justify-center items-center text-center text-primary">
-        <h1>Escolha o melhor período para suas férias!</h1>
-        <h2>Descanse, trabalhador cansado, descanse.</h2>
+      <div className="h-screen flex justify-center items-center text-center">
+        <h1 className="text-primary">Escolha o melhor período para suas férias!</h1>
+        <h2 className="text-secondary">Descanse, trabalhador cansado, descanse.</h2>
       </div>
     </>
   );
